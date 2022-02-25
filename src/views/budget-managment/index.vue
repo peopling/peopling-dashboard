0679d6dbacb1f174bc905a428e2efa7b9e125e6d
mertdemirok <mert.demirok@marti.tech>
2022-02-25 13:09:06 +0300

    Add new endpoint for 'budget'

--- src/views/budget-managment/index.vue
+++ src/views/budget-managment/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="app-container">
+    <el-button>TEST</el-button>
 
+
+     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { getBudgets, defaultBudgetData, getBudget, updateBudget, createBudget, deleteBudget } from '@/api/budgets'
+import { IBudgetData } from '@/api/types'
 
 @Component({
   name: 'budget-managment'
 })
 export default class extends Vue {
-
+  dialogFormVisible = true
 }
 </script>
